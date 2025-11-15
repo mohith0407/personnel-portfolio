c59d7c03acbbeef2aecb598511c70fd100f9eedd
Mohith <mohith.dev0407@gmail.com>
2025-11-15 17:02:29 +0530

    Add Google site verification to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,10 @@ export const metadata = {
   description: 'My portfolio built with Next.js',
   icons:{
     icon:'/favicon.png'
-  }
+  },
+  other: {
+    'google-site-verification': 'EP3ee9-7c2MwJQsz70zbQN0CVIbloYdRYeuDZ3bOHcI',
+  },
 };
 
 export default function RootLayout({ children }) {
